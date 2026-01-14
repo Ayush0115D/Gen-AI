52a57c4a7b1ebf77bc7efbfbf9066101d9454a0b
Ayush0115D <dhakreayush578@gmail.com>
2026-01-14 22:54:13 +0530

    prompting

--- 01_Tokenization/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
- # default_headers={
-    #     "HTTP-Referer": "http://localhost",
-    #     "X-Title": "GenAI Learning"
-    # }
--- 01_Tokenization/zero.py
+++ 01_Tokenization/zero.py
@@ -6,19 +6,21 @@ load_dotenv()
 
 client = OpenAI(
     api_key=os.getenv("OPENROUTER_API_KEY"),
-    base_url="https://openrouter.ai/api/v1",
+     base_url="https://openrouter.ai/api/v1"
     # default_headers={
     #     "HTTP-Referer": "http://localhost",
     #     "X-Title": "GenAI Learning"
     # } 
 )
 
+#zero-shot  prompting:Directly giving the instruction to the model without prior examples.
+SYSTEM_PROMPT = "You are a expert in coding and only answer coding related questions.Your name is alexa.if user asks non coding related questions,you will politely refuse to answer."
 response = client.chat.completions.create(
     model="google/gemini-2.5-flash",
     max_tokens=512,   # ✅ REQUIRED
     messages=[
-        {"role": "system", "content": "You are a expert in maths and ans only and only maths related questions."},
-        {"role": "user", "content": "Hey, I am Piyush Garg! Nice to meet you"}
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": "how to tell a joke?"}
     ]
 )
 
