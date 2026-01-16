95423c7b8b2cdc50132619d43f217bbaca584306
Ayush0115D <dhakreayush578@gmail.com>
2026-01-16 20:49:28 +0530

    integrating ollama with fastapi

--- FastApii/server.py
+++ FastApii/server.py
@@ -1,9 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from ollama import Client
 
-app=FastAPI()
+app = FastAPI()
+
+client = Client(
+    host="http://localhost:11434"
+)
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 @app.get("/contact-us")
 def contact_us():
-    return {"email": "Contact us at ayushd@gmail.com"}
+    return {"email": "piyushgarg.dev@gmail.com"}
+
+@app.post("/chat")
+def chat(
+    message: str = Body(..., description="The Message")
+):
+    response = client.chat(
+        model="gemma:2b",
+        messages=[
+            {"role": "user", "content": message}
+        ]
+    )
+    return {"response": response.message.content}
